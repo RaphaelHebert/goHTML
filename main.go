e1d27c05dc03a9a5ad19873ad3640125decb9ef8
raphael-corize <raphael.hebert@corize.co.jp>
2024-04-08 19:59:27 +0200

    added CSS

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func init(){
 
 func main (){
 	http.HandleFunc("/", welcome)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	// listen to port 8080 and use the default mux handler
 	log.Fatal(http.ListenAndServe(":8080", nil))
