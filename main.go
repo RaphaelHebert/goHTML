94a223c3d7f87e66ae849e7a1f2e142bc93cb625
raphael-corize <raphael.hebert@corize.co.jp>
2024-04-08 19:35:02 +0200

    serving welcome template

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+// init 
+func init(){
+	tpl = template.Must(tpl.ParseGlob("templates/*"))
+}
+
+func main (){
+	http.HandleFunc("/", welcome)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	// listen to port 8080 and use the default mux handler
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func welcome(w http.ResponseWriter, req *http.Request){
+	tpl.ExecuteTemplate(w, "welcome.gohtml", nil)
+}
