b2241752bd4e1c67cf81700d8229ce7905e61f93
raphael-corize <raphael.hebert@corize.co.jp>
2024-04-13 19:38:17 +0200

    Added comments for doc and formatted code

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
-// Main run and serve a basic webapp for made for learning purposes.
+// Package main provides HTTP handlers for serving various routes.
+// This package is written for education purposes.
+//
+// All handlers in this package follow a similar pattern:
+//   - They handle HTTP requests to specific routes.
+//   - They retrieve user information from the request.
+//   - If it's a POST request, they process uploaded text data.
+//   - They execute corresponding templates with user data.
+//
+// Parameters:
+//   - w: ResponseWriter for constructing HTTP responses.
+//   - req: Request representing an HTTP request received by the server.
+//
+// Notes:
+//   - Assumes "tpl" represents an initialized template object.
+//   - Sends appropriate HTTP error responses if file parsing or reading fails.
 
 import (
 	"html/template"
@@ -29,6 +44,9 @@ func init() {
 	tpl = template.Must(tpl.ParseGlob("templates/*"))
 }
 
+// Main is the entry point of the application. 
+// It sets up HTTP routes with corresponding handlers.
+// It starts the HTTP server to listen for incoming requests on port 8080.
 func main() {
 	http.HandleFunc("/", authorized(welcome))
 	http.HandleFunc("/login", guestGuard(login))
@@ -42,6 +60,10 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+// Login handles HTTP requests to the "/login" route. 
+// If the request method is POST, it validates the user's credentials,
+// It creates a session for the user, and redirects them to the "/welcome" page.
+// If the credentials are invalid, it returns a forbidden error.
 func login(w http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
 		// parse the file
@@ -64,12 +86,13 @@ func login(w http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(w, "login.gohtml", nil)
 }
 
+// SignUp handles HTTP requests to the "/signup" route. 
+// It processes form data submitted via POST request to create a new user account. 
+// Passwords are hashed using bcrypt for security.
+// If the email provided already exists in the user database, it returns an error response.
+// Upon successful signup, it creates a session for the new user and redirects them to the "/welcome" page.
 func signUp(w http.ResponseWriter, req *http.Request) {
-	// TODO parse form data
 	if req.Method == http.MethodPost {
-		// parse the form value
-		// create new user
-		// TODO use bcrypt to encode password
 		if _, ok := udb[req.FormValue("email")]; ok {
 			http.Error(w, "This email is already linked to an account", http.StatusForbidden)
 		}
@@ -89,6 +112,10 @@ func signUp(w http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(w, "signup.gohtml", nil)
 }
 
+// Logout handles the HTTP requests to the "/logout" route. 
+// Deletes the session associated with the user.
+// Removes the session cookie.
+// Redirects the user to the "/login" page.
 func logout(w http.ResponseWriter, req *http.Request) {
 	c, _ := req.Cookie("session")
 
@@ -106,6 +133,9 @@ func logout(w http.ResponseWriter, req *http.Request) {
 	http.Redirect(w, req, "/login", http.StatusSeeOther)
 }
 
+// Welcome handles HTTP requests to "/welcome". 
+// Welcome is the home page for logged in users.
+// Retrieve users information and execute welcome template with the user data.
 func welcome(w http.ResponseWriter, req *http.Request) {
 	u := GetUser(req)
 
@@ -129,6 +159,8 @@ func welcome(w http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(w, "welcome.gohtml", data)
 }
 
+// Authors handles HTTP requests to the "/authors" route. 
+// It renders the "authors.gohtml" template, passing the user database (udb) to be displayed.
 func authors(w http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(w, "authors.gohtml", udb)
 }
